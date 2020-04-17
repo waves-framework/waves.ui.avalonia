8720d8c65f5fb4a0c7bbfebb898798f07277e10d
Khindikaynen Egor <khek@ambertape.ru>
2020-04-17 17:34:04 +0300

    Initial commit.

---
+++ sources/Fluid.UI.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluid.UI.Avalonia.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ sources/Fluid.UI.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Fluid.UI.Avalonia.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
