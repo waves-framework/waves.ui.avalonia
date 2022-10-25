82553e383150d71dc03fd76fd437512ebef210cb
ambertape <khek@ambertape.ru>
2022-10-25 12:56:19 +0300

    .NET 7 preview and Avalonia 11 Preview

--- sources/Waves.UI.Avalonia/WavesApplication.cs
+++ sources/Waves.UI.Avalonia/WavesApplication.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using Avalonia;
-using Avalonia.Themes.Fluent;
+using FluentAvalonia.Styling;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Waves.Core;
@@ -41,7 +41,9 @@ public class WavesApplication : Application
         Core.Start();
         Core.BuildContainer();
 
-        Styles.Add(new FluentTheme(new Uri("avares://ControlCatalog/Styles")) { Mode = FluentThemeMode.Dark });
+        Styles.Add(new FluentAvaloniaTheme(new Uri("avares://ControlCatalog/Styles"))
+        {
+        });
         this.AddStyle(Constants.GenericDictionaryUri);
         this.AddStyle(_useDarkTheme ? Constants.DefaultDarkColorsUri : Constants.DefaultLightColorsUri);
 
