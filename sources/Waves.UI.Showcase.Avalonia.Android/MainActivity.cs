695cb2d3f3578e6d9c317af8197c9e9b4c409e96
Egor Khindikaynen <khek@ambertape.ru>
2022-11-03 21:25:40 +0300

    Some fixes.

---
+++ sources/Waves.UI.Showcase.Avalonia.Android/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia.Android;
+using Avalonia;
+
+namespace Waves.UI.Showcase.Avalonia.Android
+{
+    [Activity(Label = "AvaloniaApplication2.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon",
+        LaunchMode = LaunchMode.SingleInstance,
+        ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+    public class MainActivity : AvaloniaActivity<App>
+    {
+        protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+        {
+            return base.CustomizeAppBuilder(builder);
+        }
+    }
+}
